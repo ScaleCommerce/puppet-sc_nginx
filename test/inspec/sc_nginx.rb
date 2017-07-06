de9a088a1c60193f0654029fc735c1342d8f27da
Adrian Kirchner <mail@adrian-kirchner.com>
2017-07-06 15:15:27 +0200

    fix inspec test

--- test/inspec/sc_nginx.rb
+++ test/inspec/sc_nginx.rb
@@ -4,9 +4,6 @@ describe package('nginx-extras') do
 end
 
 # nginx running?
-describe service('nginx') do
- it { should be_running }
-end
 describe port(80) do
   it { should be_listening }
   its('processes') {should include 'nginx'}
