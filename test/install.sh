d1668c7dae6b23aef4cf97691817b877c5cba7e6
Thomas Lohner <tl@scale.sc>
2017-07-06 22:46:41 +0200

    add module puppet-nginx, remove Puppetfile and librarian puppet

--- test/install.sh
+++ test/install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 sed -i -e "s/nodaemon=true/nodaemon=false/" /etc/supervisord.conf
 /usr/local/bin/supervisord -c /etc/supervisord.conf
-apt-get update
-apt-get -y install curl sudo puppet ruby1.9.1-dev make gcc nano software-properties-common rsync git dnsutils unzip whois python-software-properties python-pip psmisc
-gem install --no-rdoc --no-ri librarian-puppet
-curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
+#apt-get update
+#apt-get -y install curl sudo puppet ruby1.9.1-dev make gcc nano software-properties-common rsync git dnsutils unzip whois python-software-properties python-pip psmisc
+puppet module install ajcrowe-supervisord
+puppet module install saz-sudo
+puppet module install yo61-logrotate
+git clone http://github.com/ScaleCommerce/puppet-sc_supervisor.git /etc/puppet/modules/sc_supervisor
+curl -s https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
 ln -sf /builds/sc-puppet/puppet-sc_nginx/test/hiera.yaml /etc/puppet/
 ln -sf /builds/sc-puppet/puppet-sc_nginx/test/Puppetfile /etc/puppet/
 ln -sf /builds/sc-puppet/puppet-sc_nginx/test/hiera /var/lib/hiera
 ln -sf /builds/sc-puppet/puppet-sc_nginx/ /etc/puppet/modules/sc_nginx
-cd /etc/puppet ; librarian-puppet install
 curl https://gitlab.scale.sc/scalecommerce/postinstall/raw/master/puppet.conf.sample > /etc/puppet/puppet.conf
 puppet config set certname puppet-test.scalecommerce
