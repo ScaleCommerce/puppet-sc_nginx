3d68307963f54b838f530c41b13c340b7b086934
Thomas Lohner <tl@scale.sc>
2017-07-06 22:54:45 +0200

    add apt-transport-https, remove sudo module

--- test/install.sh
+++ test/install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 sed -i -e "s/nodaemon=true/nodaemon=false/" /etc/supervisord.conf
 /usr/local/bin/supervisord -c /etc/supervisord.conf
-#apt-get update
-#apt-get -y install curl sudo puppet ruby1.9.1-dev make gcc nano software-properties-common rsync git dnsutils unzip whois python-software-properties python-pip psmisc
+apt-get install apt-transport-https
 puppet module install ajcrowe-supervisord
-puppet module install saz-sudo
 puppet module install yo61-logrotate
 git clone http://github.com/ScaleCommerce/puppet-sc_supervisor.git /etc/puppet/modules/sc_supervisor
 curl -s https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
@@ -12,5 +10,5 @@ ln -sf /builds/sc-puppet/puppet-sc_nginx/test/hiera.yaml /etc/puppet/
 ln -sf /builds/sc-puppet/puppet-sc_nginx/test/Puppetfile /etc/puppet/
 ln -sf /builds/sc-puppet/puppet-sc_nginx/test/hiera /var/lib/hiera
 ln -sf /builds/sc-puppet/puppet-sc_nginx/ /etc/puppet/modules/sc_nginx
-curl https://gitlab.scale.sc/scalecommerce/postinstall/raw/master/puppet.conf.sample > /etc/puppet/puppet.conf
+curl -s https://gitlab.scale.sc/scalecommerce/postinstall/raw/master/puppet.conf.sample > /etc/puppet/puppet.conf
 puppet config set certname puppet-test.scalecommerce
