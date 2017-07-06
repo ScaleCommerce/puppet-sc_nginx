9522162d2144dc3dd6af817ecd46b40efa35d1a8
Adrian Kirchner <mail@adrian-kirchner.com>
2017-07-06 14:52:20 +0200

    CI: Fix installer-symlinks

--- test/install.sh
+++ test/install.sh
@@ -5,10 +5,10 @@ apt-get update
 apt-get -y install curl sudo puppet ruby1.9.1-dev make gcc nano software-properties-common rsync git dnsutils unzip whois python-software-properties python-pip psmisc
 gem install --no-rdoc --no-ri librarian-puppet
 curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P inspec
-ln -sf test/hiera.yaml /etc/puppet/
-ln -sf test/Puppetfile /etc/puppet/
-ln -sf test/hiera /var/lib/hiera
-ln -sf . /etc/puppet/modules/sc_nginx
+ln -sf /builds/sc-puppet/puppet-sc_nginx/test/hiera.yaml /etc/puppet/
+ln -sf /builds/sc-puppet/puppet-sc_nginx/test/Puppetfile /etc/puppet/
+ln -sf /builds/sc-puppet/puppet-sc_nginx/test/hiera /var/lib/hiera
+ln -sf /builds/sc-puppet/puppet-sc_nginx//etc/puppet/modules/sc_nginx
 cd /etc/puppet ; librarian-puppet install
 curl https://gitlab.scale.sc/scalecommerce/postinstall/raw/master/puppet.conf.sample > /etc/puppet/puppet.conf
 puppet config set certname puppet-test.scalecommerce
