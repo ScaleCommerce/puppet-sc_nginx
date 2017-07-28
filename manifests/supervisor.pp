873ec49d0a8c0ec3880bcf8e6aca206d3c1127e7
Thomas Lohner <tl@scale.sc>
2017-07-28 16:53:48 +0200

    revert service_provider override

--- manifests/supervisor.pp
+++ manifests/supervisor.pp
@@ -1,13 +1,19 @@
 class sc_nginx::supervisor(
+  $supervisor_exec_path   = '/usr/local/bin',
 ){
 
   include sc_supervisor
   include nginx
 
-  file { ['/etc/init/nginx.conf', '/etc/init.d/nginx']:
-    ensure  => absent,
+  file { '/etc/init.d/nginx':
+    ensure => link,
+    target => "${sc_supervisor::init_path}/supervisor-init-wrapper",
     require => Package[$nginx::package_name],
-    before  => Service['nginx'],
+  }
+
+  file { '/etc/init/nginx.conf':
+    ensure => absent,
+    require => Package[$nginx::package_name],
   }
 
   file { "${supervisord::config_include}/nginx.conf":
@@ -16,13 +22,5 @@ class sc_nginx::supervisor(
     mode    => '0644',
     content => template("${module_name}/nginx.supervisor.conf.erb"),
     notify  => Class[supervisord::reload],
-    before  => Service['nginx'],
-    require => Package[$nginx::package_name],
-  }
-
-  #override puppet service provider to supervisor
-  Service <| title == "nginx" |> {
-    provider => supervisor,
-    restart => "nginx -t && nginx -s reload",
   }
 }
